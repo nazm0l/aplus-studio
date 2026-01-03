19d23c28aff9df97c563f45b19a004f2bbae918b
nipaayasha05 <nipaayasha05@gmail.com>
2026-01-03 09:57:04 +0600

    solving issue #2
    Signed-off-by: nipaayasha05 <nipaayasha05@gmail.com>

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-around space-y-5 bg-gray-50 pt-5">
+    <div className="flex justify-around space-y-5 bg-gray-50 border-b-2  pt-5">
       <div>Home</div>
       <div>What We Do</div>
       <div>Service</div>
